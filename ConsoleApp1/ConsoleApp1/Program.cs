a648636d2694d9f79c4fd08ea0d75870a4686e6d
unknown <nagyrichardmarton@gmail.com>
2024-03-27 12:34:51 +0100

    jon szi sarp fajl

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("adj meg egy szamot tesa");
+            int a =int.Parse( Console.ReadLine());
+            for (int i = 0; i < a; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
